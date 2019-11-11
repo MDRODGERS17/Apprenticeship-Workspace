04c2ae7cb1ad334a5dddf1af18501d35de73efaf
MDRODGERS17 <52426567+MDRODGERS17@users.noreply.github.com>
2019-11-10 20:03:42 -0600

    Create Library.java

---
+++ Java Practice/Library.java
@@ -0,0 +1,21 @@
+package bookstore;
+
+public class Library {
+
+	public static void main(String[] args) {
+//		books books = new books();
+//		books.Title = "Green Egss & Ham ";
+//		books.Author = "Dr. Seuss";
+//		books.Price = 6.18;
+		String [] books = {"Title: Green Eggs & Ham \r\nAuthor: Dr. Seuss\r\nPrice 6.18", "Title: Oh The Places You'll Go\r\n Author: Dr. Seuss\r\n Price: 7.85","Title: One Fish Two Fish Red Fish Blue Fish\r\nAuthor: Dr. Suess\r\nPrice: 14.77 ", "Title: How The Grinch Stole Christmas\r\nAuthor: Dr. Seuss\r\nPrice: 10.85"};
+		
+		System.out.println(books[3]);
+		
+//		System.out.println("Title: " + books.Title);
+//		System.out.println("Price: $" + books.Price);
+//		books.sell(10);
+//		
+
+	}
+
+}
